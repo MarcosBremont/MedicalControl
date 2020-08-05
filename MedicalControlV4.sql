35ec85de62932c05173004e6a7e37914babbf848
MarcosBremont <marcosbremont00@gmail.com>
2020-08-05 17:58:25 -0400

    FRMPantalla de carga agregada

--- MedicalControlV4.sql
+++ MedicalControlV4.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE  IF NOT EXISTS `medicalcontrol` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `medicalcontrol` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `medicalcontrol`;
--- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: medicalcontrol
 -- ------------------------------------------------------
--- Server version	8.0.21
+-- Server version	5.7.22-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,12 +23,12 @@ USE `medicalcontrol`;
 
 DROP TABLE IF EXISTS `t_alergia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_alergia` (
-  `IDALERGIA` int NOT NULL AUTO_INCREMENT,
+  `IDALERGIA` int(11) NOT NULL AUTO_INCREMENT,
   `NOMBREA` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`IDALERGIA`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `t_alergia` (
 
 LOCK TABLES `t_alergia` WRITE;
 /*!40000 ALTER TABLE `t_alergia` DISABLE KEYS */;
-INSERT INTO `t_alergia` VALUES (7,'AINES'),(8,'Anticonvulsivos'),(9,'Insulina'),(10,'Penicilinas'),(11,'Sulfamidas');
+INSERT INTO `t_alergia` VALUES (2,'Agua'),(3,'Azucar'),(5,'Polenn');
 /*!40000 ALTER TABLE `t_alergia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -47,22 +47,24 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_citamedica`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_citamedica` (
-  `IDCITAMEDICA` int NOT NULL AUTO_INCREMENT,
-  `NombreCompleto` varchar(60) DEFAULT NULL,
-  `HORACM` varchar(30) DEFAULT NULL,
-  `FECHACM` varchar(30) DEFAULT NULL,
-  `COMENTARIO` varchar(30) DEFAULT NULL,
-  `ID2SEGUR` int DEFAULT NULL,
-  `ID2ALERG` int DEFAULT NULL,
-  `ID2DOCTOR` int DEFAULT NULL,
+  `IDCITAMEDICA` int(11) NOT NULL AUTO_INCREMENT,
+  `HORACM` varchar(30) NOT NULL,
+  `FECHACM` varchar(30) NOT NULL,
+  `COMENTARIO` varchar(50) DEFAULT NULL,
+  `ID2SEGUR` int(11) DEFAULT NULL,
+  `ID2ALERG` int(11) DEFAULT NULL,
+  `ID2DOCTOR` int(11) DEFAULT NULL,
+  `NombreCompleto` varchar(60) NOT NULL,
   PRIMARY KEY (`IDCITAMEDICA`),
   KEY `FK2SEGURO` (`ID2SEGUR`),
   KEY `FK2DOCTOR` (`ID2DOCTOR`),
+  KEY `FK2ALERG` (`ID2ALERG`),
+  CONSTRAINT `FK2ALERG` FOREIGN KEY (`ID2ALERG`) REFERENCES `t_alergia` (`IDALERGIA`),
   CONSTRAINT `FK2DOCTOR` FOREIGN KEY (`ID2DOCTOR`) REFERENCES `t_doctor` (`IDDOCTOR`),
   CONSTRAINT `FK2SEGURO` FOREIGN KEY (`ID2SEGUR`) REFERENCES `t_segurom` (`IDSEGURO`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +73,6 @@ CREATE TABLE `t_citamedica` (
 
 LOCK TABLES `t_citamedica` WRITE;
 /*!40000 ALTER TABLE `t_citamedica` DISABLE KEYS */;
-INSERT INTO `t_citamedica` VALUES (4,'Mary Mejia','4:00:00 p. m.','11/8/2020','Paciente con problemas corazon',1,7,20);
 /*!40000 ALTER TABLE `t_citamedica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,13 +82,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_doctor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_doctor` (
-  `IDDOCTOR` int NOT NULL AUTO_INCREMENT,
+  `IDDOCTOR` int(11) NOT NULL AUTO_INCREMENT,
   `NOMBREDOCTOR` varchar(30) DEFAULT NULL,
   `ESPECIALIDAD` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`IDDOCTOR`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +97,7 @@ CREATE TABLE `t_doctor` (
 
 LOCK TABLES `t_doctor` WRITE;
 /*!40000 ALTER TABLE `t_doctor` DISABLE KEYS */;
-INSERT INTO `t_doctor` VALUES (20,'Dr/a Carlos Jimenez','Neurologo'),(21,'Dr/a Karla Perez','Endocrinologa'),(22,'Dr/a Manuel Reyes','Cardiologo'),(23,'Dr Susan Rodriguez','Neumologa'),(24,'Dra Paola Marte','Ortopeda'),(25,'Dr. Oliver Payano','Neurocirugano');
+INSERT INTO `t_doctor` VALUES (1,'Dr. Marcos','Pediatría'),(2,'Dra. Angie','of'),(3,'Carlos','Carlos'),(4,'Josefina','Josefina'),(5,'Emelyt','Emelyt'),(6,'Lola','Lola'),(8,'Polen','Polen'),(9,'Wilkania','Wilkania'),(10,'sdsd','sdsd'),(11,'cccccaaa','cccccaaa'),(12,'fdsfsdfsd','fdsfsdfsd'),(13,'fgffff','fgffff'),(14,'fsdfds','fsdfds'),(15,'dsadsa','dsadsa'),(16,'david','david'),(17,'Yeannn','saasasa'),(18,'Dr@','');
 /*!40000 ALTER TABLE `t_doctor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,16 +107,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_inventario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_inventario` (
-  `idt_inventario` int NOT NULL AUTO_INCREMENT,
+  `idt_inventario` int(11) NOT NULL AUTO_INCREMENT,
   `NombreMedicamento` varchar(60) NOT NULL,
-  `CantidadMedicamento` int DEFAULT NULL,
+  `CantidadMedicamento` int(11) DEFAULT NULL,
   `Proveedor` varchar(60) DEFAULT NULL,
-  `Descripcion` varchar(300) DEFAULT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
   `Fecha` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`idt_inventario`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,7 +125,7 @@ CREATE TABLE `t_inventario` (
 
 LOCK TABLES `t_inventario` WRITE;
 /*!40000 ALTER TABLE `t_inventario` DISABLE KEYS */;
-INSERT INTO `t_inventario` VALUES (7,'Ibuprofeno',60,'1','El ibuprofeno de venta libre se usa para bajar la fiebre y para aliviar dolores leves como dolores de cabeza, dolores musculares, artritis, cólicos menstruales, molestias del resfriado común, dolores de dientes y dolores de espalda.','2/8/2020'),(8,'Paracetamol',50,'1','Paracetamol pertenece al grupo de medicamentos llamados analgésicos y antipiréticos. Paracetamol está indicado para el tratamiento de los síntomas del dolor leve a moderado y la fiebre.','2/8/2020'),(9,'Diclofenac',80,'2','Diclofenac es una droga antiinflamatoria no esteroide (AINE o NSAID, por sus siglas en inglés). Esta medicina trabaja reduciendo sustancias en el cuerpo que causan dolor e inflamación. Diclofenac se usa para tratar el dolor leve a moderado, o los signos y síntomas de la osteoartritis','2/8/2020'),(10,'Penicilina',120,'3','La inyección de penicilina G se usa para tratar y prevenir ciertas infecciones provocadas por bacterias. La inyección de penicilina G pertenece a una clase de medicamentos llamados penicilinas. Funciona matando las bacterias que causan infecciones.','2/8/2020'),(11,'Loratadina',20,'4','La loratadina se usa para aliviar en forma temporal los síntomas de la fiebre del heno (alergia al polen, polvo u otras sustancias en el aire) y otras alergias. Estos síntomas incluyen estornudos, secreción nasal, y picazón en los ojos, nariz y garganta.\r\n','2/8/2020'),(12,'Antihipertensivos',250,'4','Los antihipertensivos son medicamentos utilizados para disminuir el riesgo cardiovascular en los pacientes con hipertensión arterial controlando la presión arterial hasta niveles adecuados. La hipertensión arterial es una enfermedad de la pared arterial de los vasos sanguíneos.\r\n','2/8/2020');
+INSERT INTO `t_inventario` VALUES (1,'Acetaminofen',16,'F San Miguel','Sirve para x cosa',NULL),(2,'Diclofenaco Sodico',28,'F El Pueblo','Sirve para esto y lo otro',NULL),(4,'Advil',59,'3','aaaaaaaaaaaaaa','29/7/2020');
 /*!40000 ALTER TABLE `t_inventario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,26 +135,26 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_paciente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_paciente` (
-  `IDPACIENTE` int NOT NULL AUTO_INCREMENT,
+  `IDPACIENTE` int(11) NOT NULL AUTO_INCREMENT,
   `NOMBREP` varchar(30) DEFAULT NULL,
   `APELLIDOP` varchar(30) DEFAULT NULL,
-  `EDADP` int DEFAULT NULL,
+  `EDADP` int(3) DEFAULT NULL,
   `CEDULAP` varchar(13) DEFAULT NULL,
   `DIRECCIONP` varchar(60) DEFAULT NULL,
   `TELEFONOP` varchar(13) DEFAULT NULL,
   `TELEFONOP2` varchar(13) DEFAULT NULL,
   `SEXOP` char(1) DEFAULT NULL,
-  `IDTALER` int DEFAULT NULL,
-  `IDTSEGURO` int DEFAULT NULL,
-  `IDTDOCTOR` int DEFAULT NULL,
+  `IDTALER` int(11) DEFAULT NULL,
+  `IDTSEGURO` int(11) DEFAULT NULL,
+  `IDTDOCTOR` int(11) DEFAULT NULL,
   PRIMARY KEY (`IDPACIENTE`),
   KEY `FKSEGURO` (`IDTSEGURO`),
   KEY `FKDOCTOR` (`IDTDOCTOR`),
   CONSTRAINT `FKDOCTOR` FOREIGN KEY (`IDTDOCTOR`) REFERENCES `t_doctor` (`IDDOCTOR`),
   CONSTRAINT `FKSEGURO` FOREIGN KEY (`IDTSEGURO`) REFERENCES `t_segurom` (`IDSEGURO`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,7 +163,7 @@ CREATE TABLE `t_paciente` (
 
 LOCK TABLES `t_paciente` WRITE;
 /*!40000 ALTER TABLE `t_paciente` DISABLE KEYS */;
-INSERT INTO `t_paciente` VALUES (13,'Omar','Bremont',19,'402-5295685-','Jarabacoa, Medina','809-556-6633','809-522-4455','M',8,5,23),(14,'Juan','Gomez',22,'402-5552221-1','Jarabacoa, La Colonia','809-556-6633','809-522-4455','M',9,6,23);
+INSERT INTO `t_paciente` VALUES (6,'Marcos','Bremont',22,'050-5259658-5','Medina','809-907-3244','809-574-6213','M',2,1,1),(7,'aa','dcccc',24,'010-4500212-5','Medina','809-907-3244','809-574-6213','M',3,1,4),(8,'','sdasdas',22,'525-4654664-6','jhgjghjghjhgjgh','565-464-5654','456-456-5465','M',2,1,1),(9,'111','ssdadas',22,'454-6546465-4','dfgdsfdsfdsfds','564-565-4654','546-454-6546','M',2,1,1),(10,'sasasa','fdfsdfsd',33,'342-3432432-4','dfdsfdsfds','343-243-2432','324-234-3242','F',2,1,1),(11,'','dsadasda',22,'323-2323232-3','fdfdsfdsfsdfsd','434-343-3434','432-423-4324','M',2,1,1),(12,'323232','dfsdfsd',344,'343-4213123-2','dfsfsdfsdf','232-323-2234','324-324-2342','M',2,1,1),(13,'fsdfsfs','dsfsfdsdfs',342,'233-2323232-3','fdsdfsdfsd','324-234-3244','342-342-4324','M',2,1,1),(14,'','GHJKJKKJ',212,'545-6456564-4','GFDSGDSGDS','465-465-4654','654-646-4564','M',2,1,1),(15,'5644','gfhdfhd',33,'453-4534534-5','fdgdfgdf','342-423-4234','654-654-6456','M',2,1,1),(16,'sdas','dsadas',22,'232-1321313-1','fdfsdfsd','213-131-2321','213-123-1231','M',2,1,1);
 /*!40000 ALTER TABLE `t_paciente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,15 +173,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_proveedor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_proveedor` (
-  `idt_proveedor` int NOT NULL AUTO_INCREMENT,
+  `idt_proveedor` int(11) NOT NULL AUTO_INCREMENT,
   `NombreProveedor` varchar(60) DEFAULT NULL,
   `UbicacionProveedor` varchar(60) DEFAULT NULL,
-  `TelefonoProveedor` varchar(13) DEFAULT NULL,
+  `TelefonoProveedor` varchar(60) DEFAULT NULL,
   `Correo` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`idt_proveedor`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -189,7 +190,7 @@ CREATE TABLE `t_proveedor` (
 
 LOCK TABLES `t_proveedor` WRITE;
 /*!40000 ALTER TABLE `t_proveedor` DISABLE KEYS */;
-INSERT INTO `t_proveedor` VALUES (1,'Laboratorios Alfa','Santiago ','808-088-8808','LaboratoriosAlfa@gmail,com'),(2,'Laboratorio Magnachem Intl. S.R.L','La Vega','809-585-8555','LaboratorioMagnachemIntl.S.R.L@gmail.com'),(3,'Ethical','Santo Domingo','809-568-5957','EthicalFarm@gmail.com'),(4,'Ibero Fármacos, SRL','Bonao','809-526-3254','IberoFármacosSRL@gmail.com');
+INSERT INTO `t_proveedor` VALUES (1,'No se x','Santiago de los caballeros','809-099-9965','Nose@gmail.com'),(3,'aaaaaa','aaaaaaaaaaa','111-111-1111','dsdsadsadasda');
 /*!40000 ALTER TABLE `t_proveedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,12 +200,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `t_segurom`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_segurom` (
-  `IDSEGURO` int NOT NULL AUTO_INCREMENT,
+  `IDSEGURO` int(11) NOT NULL AUTO_INCREMENT,
   `NOMBRESEGURO` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`IDSEGURO`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,54 +214,38 @@ CREATE TABLE `t_segurom` (
 
 LOCK TABLES `t_segurom` WRITE;
 /*!40000 ALTER TABLE `t_segurom` DISABLE KEYS */;
-INSERT INTO `t_segurom` VALUES (1,'ARS Semma'),(2,'ARS Senasa'),(3,'ARS Senasa Subsidiado'),(5,'ARS Palic Salud'),(6,'ARS Simag'),(7,'Humano'),(8,'ARS Humano');
+INSERT INTO `t_segurom` VALUES (1,'MEDICARE'),(4,'Papee');
 /*!40000 ALTER TABLE `t_segurom` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Temporary view structure for view `vtpacientetdtatsg`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `vtpacientetdtatsg`;
-/*!50001 DROP VIEW IF EXISTS `vtpacientetdtatsg`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `vtpacientetdtatsg` AS SELECT 
- 1 AS `IDPACIENTE`,
- 1 AS `NOMBREP`,
- 1 AS `APELLIDOP`,
- 1 AS `EDADP`,
- 1 AS `CEDULAP`,
- 1 AS `DIRECCIONP`,
- 1 AS `TELEFONOP`,
- 1 AS `TELEFONOP2`,
- 1 AS `SEXOP`,
- 1 AS `IDSEGURO`,
- 1 AS `NOMBRESEGURO`,
- 1 AS `IDALERGIA`,
- 1 AS `NOMBREA`,
- 1 AS `IDDOCTOR`,
- 1 AS `NOMBREDOCTOR`,
- 1 AS `ESPECIALIDAD`*/;
-SET character_set_client = @saved_cs_client;
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LoginName` varchar(100) NOT NULL,
+  `Contrasena` varchar(100) NOT NULL,
+  `FirstName` varchar(100) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  `Email` varchar(150) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `LoginName` (`LoginName`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Final view structure for view `vtpacientetdtatsg`
+-- Dumping data for table `usuario`
 --
 
-/*!50001 DROP VIEW IF EXISTS `vtpacientetdtatsg`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `vtpacientetdtatsg` AS select 1 AS `IDPACIENTE`,1 AS `NOMBREP`,1 AS `APELLIDOP`,1 AS `EDADP`,1 AS `CEDULAP`,1 AS `DIRECCIONP`,1 AS `TELEFONOP`,1 AS `TELEFONOP2`,1 AS `SEXOP`,1 AS `IDSEGURO`,1 AS `NOMBRESEGURO`,1 AS `IDALERGIA`,1 AS `NOMBREA`,1 AS `IDDOCTOR`,1 AS `NOMBREDOCTOR`,1 AS `ESPECIALIDAD` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Admin','Admin','Marcos','Bremont','Marcosbremont00@gmail.com');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -271,4 +256,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-02 15:11:43
+-- Dump completed on 2020-08-04 18:00:06
